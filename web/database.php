def036b4781fd6a6b372710198744f0c4378a98a
STPSUN <497432519@qq.com>
2018-10-18 13:57:27 +0800

    数据库

--- web/database.php
+++ web/database.php
@@ -4,8 +4,8 @@ return [
     // 数据库类型
     'type'            => 'mysql',
     // 服务器地址
-//    'hostname'        => '127.0.0.1',
-    'hostname'        => '47.98.35.18',
+    'hostname'        => '127.0.0.1',
+//    'hostname'        => '47.98.35.18',
     // 数据库名
     'database'        => 'cbc',
     // 用户名
