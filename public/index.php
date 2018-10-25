ac6c0689b6459304c84c876b49de269d15f31689
STPSUN <497432519@qq.com>
2018-10-25 11:50:34 +0800

    index

--- public/index.php
+++ public/index.php
@@ -19,8 +19,8 @@ define('ADDONS_PATH', __DIR__ . '/../addons/');
 define('UPLOADFOLDER', './uploads/');
 $url = $_SERVER['SERVER_NAME'];
 switch ($url){
-//    case "test1.ifc003.com" :
-    case "cbc.com" :
+    case "test1.ifc003.com" :
+//    case "cbc.com" :
         define('BIND_MODULE','api');
         break;
     case "www.wnct.io" :
