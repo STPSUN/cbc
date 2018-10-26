3680f680045fcb7ac68ecd29fa7385e3e1df4006
渐渐不单单 <wsas845@foxmail.com>
2018-10-26 11:19:15 +0800

    Signed-off-by: 渐渐不单单 <wsas845@foxmail.com>

---
+++ addons/member/user/controller/Balance.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace addons\member\user\controller;
+
+/**
+ * Description of Record
+ * 财富明细
+ * @author shilinqing
+ */
+class Balance extends \web\user\controller\AddonUserBase{
+    
+    public function index(){
+        $m = new \addons\config\model\BalanceConf();
+        $list = $m->getDataList(-1,-1,'','id,name','id asc');
+        $this->assign('confs',$list);
+        return $this->fetch();
+    }
+    
+    public function loadList(){
+        $keyword = $this->_get('keyword');
+        $asset_type = $this->_get('asset_type')?$this->_get('asset_type'):1;
+        $startDate = $this->_get('startDate');
+        $endDate = $this->_get("endDate");
+        $order = $this->_get("order");
+        $orderby = 'id desc';
+        if($order==1){
+            $orderby = 'amount desc';
+        }elseif($order = 2){
+            $orderby = 'amount asc';
+        }
+        $filter = '1=1';
+        if($asset_type != ''){
+            $filter .= ' and type='.$asset_type;
+        }
+        if ($keyword != null) {
+            $filter .= ' and username like \'%' . $keyword . '%\'';
+        }
+        if ($startDate != null && $endDate != null)
+            $filter .= ' and (update_time >= \'' . $startDate . ' 00:00:00\' and update_time <= \'' . $endDate . ' 23:59:59\')';
+        elseif ($startDate != null)
+            $filter .= ' and (update_time >= \'' . $startDate . ' 00:00:00\')';
+        elseif ($endDate != null)
+            $filter .= ' and (update_time <= \'' . $endDate . ' 23:59:59\')';
+        $m = new \addons\member\model\Balance();
+        $total = $m->getTotal($filter);
+        $rows = $m->getList($this->getPageIndex(), $this->getPageSize(), $filter,'*',$orderby);
+        $count_total = $m->getCountTotal($filter);
+        return $this->toTotalDataGrid($total, $rows,$count_total);
+        // return $this->toDataGrid($total, $rows);
+    }
+    
+
+    private function exportout(){
+        $all = M('user u')->where($map)->field('u.userid,username,mobile,cangku_num,fengmi_num')->join('left join ysk_store s on s.uid=u.userid')->select();
+        import("Vendor.PHPExcel.PHPExcel",'', '.php');
+        import("Vendor.PHPExcel.PHPExcel.Writer.Excel5", '', '.php');
+        import("Vendor.PHPExcel.PHPExcel.IOFactory", '', '.php');
+        $objPHPExcel = new \PHPExcel();
+        // 设置文档信息，这个文档信息windows系统可以右键文件属性查看
+        $objPHPExcel->getProperties()->setCreator("gca")
+            ->setLastModifiedBy("gca")
+            ->setTitle("gca point")
+            ->setSubject("gca point")
+            ->setDescription("gca Capital flow");
+        //根据excel坐标，添加数据
+        $objPHPExcel->setActiveSheetIndex(0)
+        ->setCellValue('A1', 'UID')
+        ->setCellValue('B1', '姓名')
+        ->setCellValue('C1', '手机号')
+        ->setCellValue('D1', '余额')
+        ->setCellValue('E1', '积分');
+        foreach($all as $k=>$v){
+            $num = $k+2;
+            $objPHPExcel->getActiveSheet()
+                ->setCellValue('A'.$num,$v['userid'])
+                ->setCellValue('B'.$num,$v['username'])
+                ->setCellValue('C'.$num,$v['mobile'])
+                ->setCellValue('D'.$num,$v['cangku_num'])
+                ->setCellValue('E'.$num,$v['fengmi_num']);
+            
+        }
+        // 设置第一个sheet为工作的sheet
+        $objWriter = \PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel2007');
+        $filename = date('Y-m-d').'.xlsx';
+        // 保存Excel 2007格式文件，保存路径为当前路径，名字为export.xlsx
+        ob_end_clean();     //清除缓冲区,避免乱码
+        header("Content-Type: application/force-download");
+        header("Content-Type: application/octet-stream");
+        header("Content-Type: application/download");
+        header('Content-Disposition:inline;filename="'.$filename.'"');
+        header("Content-Transfer-Encoding: binary");
+        header("Cache-Control: must-revalidate, post-check=0, pre-check=0");
+        header("Pragma: no-cache");
+        $objWriter->save('php://output');
+    }
+}
