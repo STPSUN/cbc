ed9be266b1b5e39f7f25634df2750462f6e88d67
渐渐不单单 <wsas845@foxmail.com>
2018-10-27 11:04:41 +0800

    Signed-off-by: 渐渐不单单 <wsas845@foxmail.com>

---
+++ addons/member/user/controller/Leave.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace addons\member\user\controller;
+
+/**
+ @author zhuangminghan 
+ * 用户留言
+ */
+class Leave extends \web\user\controller\AddonUserBase {
+
+    public function index() {
+        $type = $this->_get('type');
+        if ($type == '') {
+            $type = 0;
+        }
+        $this->assign('type', $type);
+        return $this->fetch();
+    }
+
+    public function loadList() {
+        $keyword = $this->_get('keyword');
+        $filter = ' 1=1';
+        if ($keyword != null) {
+            $filter .= ' and sphone like "%' . $keyword . '%" or order_id like "%' . $keyword . '%"';
+        }
+        $r = new \addons\member\model\TradingComplaint();
+        $total = $r->getTrandTotal($filter);
+        $rows = $r->getList($filter,$this->getPageIndex(), $this->getPageSize());
+        return $this->toDataGrid($total, $rows);
+    }
+
+    public function loadData(){
+        $id = $this->_get('id');
+        $m = new \addons\member\model\TradingComplaint();
+        $data = $m->getDetail($id);
+        return $data;
+    }
+
+    public function edit() {
+        if (IS_POST) {
+            $data = $_POST;
+            $id = $data['id'];
+            $data['update_at'] = NOW_DATETIME;
+            $data['type'] = 1;
+            $m = new \addons\member\model\TradingComplaint();
+            try {
+                $ret = $m->save($data);
+                return $this->successData();
+            } catch (\Exception $e) {
+                return $this->failData($e->getMessage()); 
+            }
+        } else {
+            $this->assign('id', $this->_get('id'));
+            $m = new \addons\member\model\TradingComplaint();
+            $role = $m->getDataList();
+            $this->assign('role',$role);
+            $this->setLoadDataAction('loadData');
+            return $this->fetch();
+        }
+    }
+
+    /**
+    * 操作投诉
+    */
+    public function cancleOrder(){
+        $r = new \addons\member\model\TradingComplaint();
+        $id = $this->_get('id');
+        $res = $r->where(['id'=>$id])->update(['type'=>1]);
+        if($res){
+            return $this->successData('操作成功');
+        }else{
+            return $this->failData('操作失败');
+        }
+
+    }
+}
