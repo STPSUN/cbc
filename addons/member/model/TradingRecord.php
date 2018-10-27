40ec634f7f9114350c96ec0762b3fc8a1b877438
STPSUN <497432519@qq.com>
2018-10-27 14:10:42 +0800

    交易明细

--- addons/member/model/TradingRecord.php
+++ addons/member/model/TradingRecord.php
@@ -130,12 +130,12 @@ class TradingRecord extends \web\common\model\BaseModel{
      * @param $order
      * @return mixed
      */
-    public function getRecordList($asset_type,$pageIndex=1,$pageSize=10,$order)
+    public function getRecordList($asset_type,$pageIndex=1,$pageSize=10,$user_id,$order)
     {
         $sql = "SELECT u.phone,r.amount,r.type,r.change_type,r.type,r.update_time "
             . " FROM tp_trading_record AS r "
             . " JOIN tp_member_account AS u ON u.id = r.to_user_id "
-            . " WHERE r.asset_type = $asset_type";
+            . " WHERE r.asset_type = $asset_type and r.user_id = $user_id";
 
         return $this->getDataListBySQL($sql, $pageIndex, $pageSize, $order);
     }
