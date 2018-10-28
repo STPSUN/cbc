abd1c07449432fcc1115db3294ce881e2f67185d
渐渐不单单 <wsas845@foxmail.com>
2018-10-28 18:51:37 +0800

    Signed-off-by: 渐渐不单单 <wsas845@foxmail.com>

--- web/api/controller/Wallet.php
+++ web/api/controller/Wallet.php
@@ -265,10 +265,11 @@ class Wallet extends ApiBase
     public function income()
     {
         $recordM = new TradingRecord();
+        $memberNode = web\api\model\MemberNodeIncome();
         $trading_amount = $recordM->where(['user_id' => $this->user_id, 'type' => 9])->sum('amount');
         $share_amount = $recordM->where(['user_id' => $this->user_id, 'type' => 10])->sum('amount');
         $peer_amount = $recordM->where(['user_id' => $this->user_id, 'type' => 11])->sum('amount');
-        $node_amount = $recordM->where(['user_id' => $this->user_id, 'type' => 14])->sum('amount');
+        $node_amount = $memberNode->->where(['user_id' => $this->user_id])->sum('amount');
 
         $data = array(
             // 'team_amount'       => 0,
