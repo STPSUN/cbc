77e2bb145ab9bc625e2943cd32936c5cd898ad5a
STPSUN <497432519@qq.com>
2018-10-28 12:21:31 +0800

    节点释放记录

--- web/api/service/NodeService.php
+++ web/api/service/NodeService.php
@@ -23,8 +23,9 @@ class NodeService extends \web\common\controller\Service
      */
     public function nodeRelease()
     {
-        $users = $this->getNodeUsers();
-        foreach ($users as $v)
+        $nodes = $this->getNodeUsers();
+//        print_r($nodes);exit();
+        foreach ($nodes as $v)
         {
             $this->release($v);
         }
@@ -33,53 +34,67 @@ class NodeService extends \web\common\controller\Service
     /**
      * 用户节点释放
      */
-    private function release($user_id)
+    private function release($member_node)
     {
         $nodeServiceM = new NodeService();
-        $release = $nodeServiceM->getReleaseNum($user_id);
+//        $release = $nodeServiceM->getReleaseNum($member_node);
 
         $recordM = new TradingRecord();
         $balanceM = new Balance();
         $incomeM = new MemberNodeIncome();
 
+        $user_id = $member_node['user_id'];
+        $amount = $member_node['release_num'];
+        $member_node_id = $member_node['node_id'];
+        $node_type = $member_node['type'];
+
         $recordM->startTrans();
         try
         {
             $remark = '节点释放';
-            if($release['normal_num'])
+            if($node_type < 8)
             {
                 Log::record("开始释放普通节点，user_id:" . $user_id);
                 //总额
                 $total_balance = $balanceM->getBalanceAmountByType($user_id,1);
-                $total_balance_after = bcadd($total_balance,$release['normal_num'],2);
-                $recordM->addRecord(0,$release['normal_num'],$total_balance,$total_balance_after,1,13,1,$user_id,$remark);
-                $balanceM->updateBalance($user_id,1,$release['normal_num'],true);
+                $total_balance_after = bcadd($total_balance,$amount,2);
+                $recordM->addRecord(0,$amount,$total_balance,$total_balance_after,1,13,1,$user_id,$remark);
+                $balanceM->updateBalance($user_id,1,$amount,true);
 
                 //可用余额
                 $use_balance = $balanceM->getBalanceAmountByType($user_id,2);
-                $use_amount = bcmul($release['normal_num'],0.7,2);
+                $use_amount = bcmul($amount,0.7,2);
                 $use_balance_after = $use_balance + $use_amount;
                 $recordM->addRecord(0,$use_amount,$use_balance,$use_balance_after,2,13,1,$user_id,$remark);
                 $balanceM->updateBalance($user_id,2,$use_amount,true);
 
                 Log::record("普通节点释放成功，user_id:" . $user_id);
-            }
-
-            if($release['super_num'])
+            }else
             {
                 Log::record("开始释放超级节点，user_id:" . $user_id);
                 //超级节点释放
                 $key_balance = $balanceM->getBalanceAmountByType($user_id,4);
-                $key_amount = bcmul($release['super_num'],0.7,2);
+                $key_amount = bcmul($amount,0.7,2);
                 $key_balance_after = $key_balance + $key_amount;
                 $recordM->addRecord(0,$key_amount,$key_balance,$key_balance_after,4,13,1,$user_id,$remark);
                 $balanceM->updateBalance($user_id,4,$key_amount,true);
 
                 $awardS = new AwardService();
-                $amount = bcmul($release['super_num'],0.3,2);
+                $amount = bcmul($amount,0.3,2);
                 $awardS->tradingReward($amount,$user_id);
                 Log::record("超级节点释放成功，user_id:" . $user_id);
             }
+
+            $data = array();
+            $data = array(
+                'member_node_id'    => $member_node_id,
+                'create_time'       => NOW_DATETIME,
+                'amount'            => $amount,
+                'type'              => $node_type,
+                'user_id'           => $user_id,
+            );
+
+            $incomeM->add($data);
 
             $recordM->commit();
             return true;
@@ -98,10 +113,9 @@ class NodeService extends \web\common\controller\Service
     {
         $memberNodeM = new MemberNode();
         $map['pass_time'] = array('>=',time());
-        $users = $memberNodeM->where($map)->column('user_id');
-        $users = array_unique($users);
+        $nodes = $memberNodeM->field('node_id,release_num,type,user_id')->where($map)->select();
 
-        return $users;
+        return $nodes;
     }
 
     /**
