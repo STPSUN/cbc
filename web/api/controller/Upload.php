5c567284a973595663aa94cf4c7c0c77d8f3121f
渐渐不单单 <wsas845@foxmail.com>
2018-10-29 09:55:29 +0800

    Signed-off-by: 渐渐不单单 <wsas845@foxmail.com>

--- web/api/controller/Upload.php
+++ web/api/controller/Upload.php
@@ -48,48 +48,56 @@ class Upload extends ApiBase
     /**
      * 上传身份证正面
      */
-    public function id_face(){
+    public function id_card(){
         $user_id = $this->user_id;
         if(!$user_id) return $this->failJSON(lang('COMMON_LOGIN'));
         $base64 = $this->_post('image');
+        $type = $this->_post('type');
         if($base64){
-            $savePath = 'user/id_face/';
+            if($type=='id_face'){
+                $savePath = 'user/id_face/';
+                $str = lang('UPLOAD_FACE_SUCCESS');
+            }else{
+                $savePath = 'user/id_back/';
+                $str = lang('UPLOAD_BACK_SUCCESS');
+            }
             $ret = $this->base_img_upload($base64, $this->user_id, $savePath);
             if(!$ret['success']){
                 return $this->failJSON($ret['message']);
             }
 
             $m = new \addons\member\model\MemberAccountModel();
-            $res = $m->where(['id'=>$user_id])->update(['id_face'=>$ret['path'],'is_auth'=>2]);
+            $res = $m->where(['id'=>$user_id])->update([$type=>$ret['path'],'is_auth'=>2]);
             if($res){
-                return $this->successJSON(lang('UPLOAD_FACE_SUCCESS'));
+
+                return $this->successJSON($str);
             }else{
                 return $this->failJSON($ret['message']);
             }
         }
     }
 
-    /**
-     * 上传身份证反面
-     */
-    public function id_back(){
-        $user_id = $this->user_id;
-        if(!$user_id) return $this->failJSON(lang('COMMON_LOGIN'));
-        $base64 = $this->_post('image');
-        if($base64){
-            $savePath = 'user/id_back/';
-            $ret = $this->base_img_upload($base64, $this->user_id, $savePath);
-            if(!$ret['success']){
-                return $this->failJSON($ret['message']);
-            }
+    // /**
+    //  * 上传身份证反面
+    //  */
+    // public function id_back(){
+    //     $user_id = $this->user_id;
+    //     if(!$user_id) return $this->failJSON(lang('COMMON_LOGIN'));
+    //     $base64 = $this->_post('image');
+    //     if($base64){
+    //         $savePath = 'user/id_back/';
+    //         $ret = $this->base_img_upload($base64, $this->user_id, $savePath);
+    //         if(!$ret['success']){
+    //             return $this->failJSON($ret['message']);
+    //         }
 
-            $m = new \addons\member\model\MemberAccountModel();
-            $res = $m->where(['id'=>$user_id])->update(['id_back'=>$ret['path'],'is_auth'=>2]);
-            if($res){
-                return $this->successJSON(lang('UPLOAD_BACK_SUCCESS'));
-            }else{
-                return $this->failJSON($ret['message']);
-            }
-        }
-    }
+    //         $m = new \addons\member\model\MemberAccountModel();
+    //         $res = $m->where(['id'=>$user_id])->update(['id_back'=>$ret['path'],'is_auth'=>2]);
+    //         if($res){
+    //             return $this->successJSON(lang('UPLOAD_BACK_SUCCESS'));
+    //         }else{
+    //             return $this->failJSON($ret['message']);
+    //         }
+    //     }
+    // }
 }
