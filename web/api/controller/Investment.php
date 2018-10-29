e85522f2bd120b91d037ac1baacb7304af3d3bf2
渐渐不单单 <wsas845@foxmail.com>
2018-10-29 13:14:08 +0800

    Signed-off-by: 渐渐不单单 <wsas845@foxmail.com>

--- web/api/controller/Investment.php
+++ web/api/controller/Investment.php
@@ -83,7 +83,7 @@ class Investment extends ApiBase
                 return $this->failJSON(lang('INVESTMENT_REDUCE_WRONG'));
             }
             $AwardService = new \web\api\service\AwardService();
-            $fee_num = $amount['amount']/7*3;
+            $fee_num = $amount/7*3;
             $res = $AwardService->tradingReward($fee_num,$user_id);
             //计算奖金
             if(!$res){
