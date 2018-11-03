6e6643f801a2e83bb9a7df0ac53b621090ca4d11
渐渐不单单 <wsas845@foxmail.com>
2018-11-03 13:57:58 +0800

    Signed-off-by: 渐渐不单单 <wsas845@foxmail.com>

--- addons/member/model/Trading.php
+++ addons/member/model/Trading.php
@@ -39,9 +39,10 @@ class Trading extends \web\common\model\BaseModel{
             ->join("member_pay_config p",'p.user_id=t.user_id','LEFT')->order($order)
             ->limit($page,$size)->select();
         }else{
-            $list =  $this->alias('t')->field('t.*,m.phone,m.credit_level user_level,m.username,m.head_img,m.is_auth')->where($map)
+            $list =  $this->fetchsql()->alias('t')->field('t.*,m.phone,m.credit_level user_level,m.username,m.head_img,m.is_auth')->where($map)
             ->join("member_account m",'m.id=t.user_id','LEFT')->order($order)
             ->limit($page,$size)->select();
+            echo $list;exit();
         }
         foreach ($list as $key => $value) {
             $info = $this->table('tp_member_pay_config')->where(['user_id'=>$value['user_id']])->select();
