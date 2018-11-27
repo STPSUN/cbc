b37f4b5fed844c0a241ced1b66817ab8b5694e65
渐渐不单单 <wsas845@foxmail.com>
2018-11-27 14:49:56 +0800

    Signed-off-by: 渐渐不单单 <wsas845@foxmail.com>

--- web/api/controller/Crontab.php
+++ web/api/controller/Crontab.php
@@ -301,6 +301,7 @@ class Crontab extends \web\common\controller\Controller {
         }else{
             $page = 0;
         }
+        echo '---'.$page.'---';
         $redis->set('release_page',$page);
         $map['type'] = ['in','2,3,4,5,6,7'];
         $allnode = $nodeS->field('id,type,user_id,sum(node_num) as node_num,sum(total_num) as total_num')->where($map)->group('user_id')->limit($page,500)->select();
